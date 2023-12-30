5848d25c1101843383a52c9ed2f37a27805b697e
MarcoCarvalhoDev <dev.carvalho1990@gmail.com>
2023-12-30 01:17:58 -0300

    Novos arquivos inseridos - até o módulo 04: Operadores

---
+++ operadorTernario.js
@@ -0,0 +1,5 @@
+// 
+const idadeMinima = 18;
+let idadeCliente = 2;
+
+console.log(idadeCliente >= idadeMinima ? "Cliente maior de idade. Venda autorizada." : "Cliente menor de idade. Venda proibida.")
